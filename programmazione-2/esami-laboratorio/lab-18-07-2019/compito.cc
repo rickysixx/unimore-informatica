c89f976264d5b61c73b24d4aff187283478c54b1
rickysixx <51907568+rickysixx@users.noreply.github.com>
2021-07-16 12:36:06 +0200

    punto 3.b

--- programmazione-2/esami-laboratorio/lab-18-07-2019/compito.cc
+++ programmazione-2/esami-laboratorio/lab-18-07-2019/compito.cc
@@ -127,6 +127,41 @@ void stampa_most_influential_people(const graph& grafo, const node* nodi, const 
     }
 }
 
+void follow(const graph& grafo, const node* nodi, const int id_sorgente)
+{
+    bool* visited = new bool[grafo.dim];
+
+    for (int i = 0; i < grafo.dim; i++)
+    {
+        visited[i] = false;
+    }
+
+    coda coda = newQueue();
+
+    coda = enqueue(coda, id_sorgente);
+    visited[id_sorgente - 1] = true;
+
+    while (!isEmpty(coda))
+    {
+        int id_nodo = dequeue(coda);
+        adj_node* iteratore = get_adjlist(grafo, id_nodo);
+
+        while (iteratore != nullptr)
+        {
+            int adjnode_id = get_adjnode(iteratore);
+
+            if (!visited[adjnode_id - 1] && nodi[adjnode_id - 1].tipo == TIPO_UTENTE)
+            {
+                std::cout << nodi[adjnode_id - 1].cont << std::endl;
+                coda = enqueue(coda, adjnode_id);
+                visited[adjnode_id - 1] = true;
+            }
+
+            iteratore = get_nextadj(iteratore);
+        }
+    }
+}
+
 int main()
 {
     std::ifstream file_grafo(GRAFO_NOME_FILE);
@@ -169,6 +204,15 @@ int main()
             
             std::cout << "I most influential people sono:" << std::endl;
             stampa_most_influential_people(grafo, nodi, numero_like_per_utente);
+            std::cout << std::endl;
+
+            int sorgente_follow;
+
+            std::cout << "Inserisci l'identificativo del nodo di un utente di cui stampare i follow: ";
+            std::cin >> sorgente_follow;
+
+            std::cout << "L'utente " << nodi[sorgente_follow - 1].cont << " segue:" << std::endl;
+            follow(grafo, nodi, sorgente_follow);
 
             return 0;
         }
