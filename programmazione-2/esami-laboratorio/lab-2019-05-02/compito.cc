504b012a50f306b2b023626a5ef6bf65f3cc41e0
rickysixx <51907568+rickysixx@users.noreply.github.com>
2021-07-19 11:52:37 +0200

    punto 2

--- programmazione-2/esami-laboratorio/lab-2019-05-02/compito.cc
+++ programmazione-2/esami-laboratorio/lab-2019-05-02/compito.cc
@@ -33,6 +33,54 @@ void stampa(lista lista)
     }
 }
 
+carta* scala(lista carte, int& lunghezza)
+{
+    carta* prima_carta_scala = nullptr;
+    lunghezza = 0;
+    elem* prima_carta_scala_corrente = carte;
+    int lunghezza_scala_corrente = 1;
+
+    for (elem* it = tail(carte); it != nullptr; it = tail(it))
+    {
+        if (prima_carta_scala_corrente->inf.seme == head(it).seme && head(it).valore == prev(it)->inf.valore + 1)
+        { 
+            ++lunghezza_scala_corrente;
+        }
+        else
+        {
+            if (lunghezza_scala_corrente >= 3 && lunghezza_scala_corrente > lunghezza)
+            {
+                prima_carta_scala = &prima_carta_scala_corrente->inf;
+                lunghezza = lunghezza_scala_corrente;
+            }
+
+            prima_carta_scala_corrente = it;
+            lunghezza_scala_corrente = 1;
+        }
+
+        if (lunghezza_scala_corrente >= 3 && lunghezza_scala_corrente > lunghezza)
+        {
+            prima_carta_scala = &prima_carta_scala_corrente->inf;
+            lunghezza = lunghezza_scala_corrente;
+        }
+    }
+
+    return prima_carta_scala;
+}
+
+void stampa_scala(const lista& lista, const carta* prima_carta_scala, const int lunghezza_scala)
+{
+    elem* it = search(lista, *prima_carta_scala);
+
+    for (int i = 0; i < lunghezza_scala; i++)
+    {
+        print(head(it));
+        std::cout << " ";
+
+        it = tail(it);
+    }
+}
+
 int main()
 {
     lista giocatore_1 = nullptr;
@@ -56,6 +104,21 @@ int main()
 
     std::cout << "giocatore 2: ";
     stampa(giocatore_2);
+
+    int lunghezza_scala_giocatore_1;
+    int lunghezza_scala_giocatore_2;
+    carta* scala_giocatore_1 = scala(giocatore_1, lunghezza_scala_giocatore_1);
+    carta* scala_giocatore_2 = scala(giocatore_2, lunghezza_scala_giocatore_2);
+
+    std::cout << "Scale:" << std::endl;
+
+    std::cout << "giocatore 1: ";
+    stampa_scala(giocatore_1, scala_giocatore_1, lunghezza_scala_giocatore_1);
+    std::cout << std::endl;
+
+    std::cout << "giocatore 2: ";
+    stampa_scala(giocatore_2, scala_giocatore_2, lunghezza_scala_giocatore_2);
+    std::cout << std::endl;
 
     return 0;
 }
