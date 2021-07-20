93972314517414d95f0e16ee2e80aa4071c8f697
rickysixx <51907568+rickysixx@users.noreply.github.com>
2021-07-20 09:33:32 +0200

    punto 2.a

--- programmazione-2/esami-laboratorio/lab-2019-05-02/compito.cc
+++ programmazione-2/esami-laboratorio/lab-2019-05-02/compito.cc
@@ -81,6 +81,33 @@ void stampa_scala(const lista& lista, const carta* prima_carta_scala, const int 
     }
 }
 
+int cala(lista& carte)
+{
+    int numero_carte_scala;
+    elem* it = search(carte, *scala(carte, numero_carte_scala));
+    int punteggio = 0;
+
+    std::cout << "Carte calate: ";
+
+    for (int i = 0; i < numero_carte_scala; i++)
+    {
+        elem* next = tail(it);
+        carta carta = head(it);
+
+        print(carta);
+        std::cout << " ";
+
+        punteggio += carta.valore;
+        
+        carte = delete_elem(carte, it);
+        it = next;
+    }
+
+    std::cout << std::endl;
+
+    return punteggio;
+}
+
 int main()
 {
     lista giocatore_1 = nullptr;
@@ -119,6 +146,64 @@ int main()
     std::cout << "giocatore 2: ";
     stampa_scala(giocatore_2, scala_giocatore_2, lunghezza_scala_giocatore_2);
     std::cout << std::endl;
+
+    const unsigned int nr_turni_gioco = 2;
+    int vincitore = -1;
+    int punteggio_giocatore_1 = 0;
+    int punteggio_giocatore_2 = 0;
+
+    for (unsigned int i = 1; i <= nr_turni_gioco && vincitore == -1; i++)
+    {
+        std::cout << "=== TURNO " << i << " ===" << std::endl;
+
+        std::cout << "giocatore 1, pesca una carta:" << std::endl;
+        pesca(giocatore_1);
+
+        std::cout << "giocatore 1 cala le carte:" << std::endl;
+        punteggio_giocatore_1 += cala(giocatore_1);
+        std::cout << "Punteggio totale giocatore 1: " << punteggio_giocatore_1 << std::endl;
+
+        if (giocatore_1 == nullptr)
+        {
+            vincitore = 1;
+        }
+        else
+        {
+            std::cout << "giocatore 1, carte in mano: ";
+            stampa(giocatore_1);
+
+            std::cout << "giocatore 2, pesca una carta:" << std::endl;
+            pesca(giocatore_2);
+
+            std::cout << "giocatore 2 cala le carte:" << std::endl;
+            punteggio_giocatore_2 += cala(giocatore_2);
+            std::cout << "Punteggio totale giocatore 2: " << punteggio_giocatore_2 << std::endl;
+
+            if (giocatore_2 == nullptr)
+            {
+                vincitore = 2;
+            }
+            else
+            {
+                std::cout << "giocatore 2, carte in mano: ";
+                stampa(giocatore_2);
+            }
+        }
+    }
+
+    if (vincitore == -1)
+    {
+        vincitore = (punteggio_giocatore_1 > punteggio_giocatore_2) ? 1 : 2;
+    }
+
+    if (vincitore == 1)
+    {
+        std::cout << "Fine gioco!! Vince il giocatore 1 con " << punteggio_giocatore_1 << " punti." << std::endl;
+    }
+    else
+    {
+        std::cout << "Fine gioco!! Vince il giocatore 2 con " << punteggio_giocatore_2 << " punti." << std::endl;
+    }
 
     return 0;
 }
